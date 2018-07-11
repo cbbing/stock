57309eed18ea8d6a8df2bf362aa41d7b8e1f41da
Li Shulin <1627041882@qq.com>
2018-07-11 21:28:51 +0800

    Update macd_back_test.py

--- trade_process/strategy/macd_back_test.py
+++ trade_process/strategy/macd_back_test.py
@@ -665,6 +665,9 @@ def macdmain(stockName,fundlist):
     # fundlist = efund_mail2.get_histrydata(stockName, 180)
     stock_data = pd.DataFrame(fundlist[:200][::-1], columns=['Date', 'Adj Close', 'countclose', 'change'])
     # self_adaptive_ma(stock_data)
+    period_type = 'M'
+    stock_data = stock_data.resample(period_type,how='last')
+    #stock_data = 
 
     Avg1=([3,8,40])
     # select_method=['MA','MACD','DMA','TRIX','AMA']
